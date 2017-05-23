fbb8a26d67e5f420516312b5834ac46443540af1
Henryk Konsek <hekonsek@gmail.com>
2017-05-23 16:21:29 +0200

    Added base Kafka test.

--- apps/timer/src/test/java/reflow/app/timer/BaseKafkaTest.java
+++ apps/timer/src/test/java/reflow/app/timer/BaseKafkaTest.java
@@ -1,5 +1,7 @@
 package reflow.app.timer;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -8,13 +10,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.test.rule.KafkaEmbedded;
-import org.springframework.kafka.test.utils.KafkaTestUtils;
 
-import java.util.UUID;
-
+import static java.util.Collections.singletonList;
+import static org.springframework.kafka.test.utils.KafkaTestUtils.consumerProps;
 import static org.springframework.kafka.test.utils.KafkaTestUtils.producerProps;
 
 @Configuration
@@ -22,6 +22,26 @@ public class BaseKafkaTest {
 
     @ClassRule
     public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(1, true, "vehicle");
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.setProperty("spring.kafka.bootstrapServers", embeddedKafka.getBrokersAsString());
+        System.setProperty("spring.cloud.stream.kafka.binder.zkNodes", embeddedKafka.getZookeeperConnectionString());
+    }
+
+    @Autowired
+    protected ApplicationContext applicationContext;
+
+    protected <K,V> Consumer<K, V> consumer(String topic) {
+        Consumer<K,V> consumer = applicationContext.getBean(ConsumerFactory.class).createConsumer();
+        consumer.subscribe(singletonList(topic));
+        return consumer;
+    }
+
+    @Bean
+    ConsumerFactory<?, ?> kafkaConsumerFactory() {
+        return new DefaultKafkaConsumerFactory<Object, Object>(consumerProps("test", "true", embeddedKafka));
+    }
 
     @Bean
     ProducerFactory<?, ?> kafkaProducerFactory() {
--- apps/timer/src/test/java/reflow/app/timer/ReflowAppTimerApplicationTest.java
+++ apps/timer/src/test/java/reflow/app/timer/ReflowAppTimerApplicationTest.java
@@ -5,12 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.springframework.kafka.test.utils.KafkaTestUtils.getSingleRecord;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = ReflowAppTimerApplicationTest.class)
+@SpringBootTest(classes = {ReflowAppTimerApplicationTest.class, ReflowAppTimerApplication.class})
 public class ReflowAppTimerApplicationTest extends BaseKafkaTest {
 
     @Test
-    public void should() {
+    public void shouldSendMessageToTopic() {
+        getSingleRecord(consumer("vehicle"), "vehicle");
     }
 
 }
